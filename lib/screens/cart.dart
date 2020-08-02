fe4e1ffdc6121a1dc73da88e20ae59901a7ba8f4
Shalomi <you@example.com>
2020-08-01 17:15:18 -0700

    cart item created

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,64 +1,49 @@
-import 'package:application/providers/cart_products.dart';
-import 'package:application/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 
-class CartScreen extends StatefulWidget {
+//my imports
+import 'package:eevokclothing/providers/cart_products.dart';
+//import 'package:eevokclothing/cart_products.dart';
+
+
+
+class Cart extends StatefulWidget {
   @override
-  _CartScreenState createState() => _CartScreenState();
+  _CartState createState() => _CartState();
 }
 
-class _CartScreenState extends State<CartScreen> {
+class _CartState extends State<Cart> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+       appBar: new AppBar(
+         elevation: 0.1,
         backgroundColor: Colors.pink,
-        title: Text(
-          "Your Cart",
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-          ),
-        ],
+       title:Text('cart'),
+       actions: <Widget>[
+        new IconButton(icon: Icon(Icons.search,color:Colors.white), onPressed: (){}),
+        
+       ], 
       ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          color: Colors.white,
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                child: ListTile(
-                  title: Text("Total : "),
-                  subtitle: Text("\$240"),
-                ),
-              ),
-              Expanded(
-                child: MaterialButton(
-                  padding: EdgeInsets.all(15.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  elevation: 5.0,
-                  color: Colors.red,
-                  onPressed: () {},
-                  child: Text(
-                    "Checkout",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ],
+      
+      body:new Cart_products() ,
+
+      bottomNavigationBar: new Container(
+        color:Colors.white,
+        child:Row(
+          children: <Widget>[
+          Expanded(child: ListTile(
+            title: new Text("Total"),
+            subtitle: new Text("\$230"),
+            )),
+
+            Expanded(
+            child: new MaterialButton(onPressed: (){},
+            child: new Text("Check out"),
+            color: Colors.pink,),
+            )
+          ],
           ),
-        ),
-      ),
-      body: CartProducts(),
-      drawer: SideDrawer(),
+      ), 
     );
   }
 }
